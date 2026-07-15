ba981061f2b5f9aac83905d26412c973e3debdc4
Karena Ayushi <karenaaayushi19@gmail.com>
2026-07-15 18:00:01 +0530

    Update settings.py

--- Assignment-4/Session-10/emailproject/emailproject/settings.py
+++ Assignment-4/Session-10/emailproject/emailproject/settings.py
@@ -106,7 +106,7 @@ EMAIL_PORT = 587
 
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = "miranena1017@gmail.com"
+EMAIL_HOST_USER = "karenaaayushi19@gmail.com"
 
 EMAIL_HOST_PASSWORD = "brea jbnc ajyz hhjn"
 
